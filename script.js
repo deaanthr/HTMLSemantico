2bf301f43a7b0bd1f0c73f3842b8e534551ae887
Joao A <joaobarao1803@gmail.com>
2026-04-09 17:15:21 -0300

    Atualizações da Aula

--- script.js
+++ script.js
@@ -7,3 +7,47 @@ function alterarNome() {
         document.getElementById('nomeUsuario').innerHTML = trocarNome;
     }
 }
+
+function alterarCurso () {
+    let trocarCurso = document.getElementById('inputCurso').value;
+
+    if (trocarCurso == '') {
+        alert ('Digite um curso!')
+    } else {
+        document.getElementById('nomeCurso').innerHTML = trocarCurso
+    }
+}
+
+function alterarStatus () {
+    let trocarCurso = document.getElementById('inputStatus').value;
+
+    if (trocarCurso == '') {
+        alert ('Digite um curso!')
+    } else {
+        document.getElementById('status').innerHTML = trocarCurso
+    }
+}
+
+function mudarCorHeader() {
+    let cor = document.getElementById('corHeader').value;
+
+    document.querySelector('.cabecalho').style.backgroundColor = cor;
+}
+
+function mudarCorNav() {
+    let cor = document.getElementById('corNav').value;
+
+    document.querySelector('.navegacao').style.backgroundColor = cor;
+}
+
+function mudarCorSection() {
+    let cor = document.getElementById('corSection').value;
+    
+    document.querySelector('.secao').style.backgroundColor = cor;
+}
+
+function mudarCorFooter() {
+    let cor = document.getElementById('corFooter').value;
+
+    document.querySelector('.rodape').style.backgroundColor = cor;
+}
